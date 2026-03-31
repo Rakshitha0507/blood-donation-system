d752e306885bdcb3d357f25dd2f18e73e0a6a680
Rakshitha0507 <rakshitanarukudubindela@gmail.com>
2026-03-31 20:17:57 +0530

    fix api frotned

--- blood-backend/src/main/java/com/example/demo/security/SecurityConfig.java
+++ blood-backend/src/main/java/com/example/demo/security/SecurityConfig.java
@@ -58,7 +58,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
+        configuration.setAllowedOrigins(Arrays.asList("https://blood-donation-system-production-e959.up.railway.app"));
         configuration.setAllowedMethods(Arrays.asList(
                 "GET", "POST", "PUT", "DELETE", "OPTIONS"
         ));
